2993619eea6f1a0b68394bcea6b84684429e2d60
Joy Gabriella <joysan05@outlook.com>
2023-01-23 22:38:46 -0300

    Commit melhorado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var ul = document.querySelector('nav ul');
 var menuBtn = document.querySelector('.icone i');
 
+
 function menuShow(){
     if(ul.classList.contains('open')){
         ul.classList.remove('open');
     }else{
         ul.classList.add('open');
     }
+
+   
 }
